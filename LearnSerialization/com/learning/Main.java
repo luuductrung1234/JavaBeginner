8a50e5f92f6006ea015ef1e35f6c53316ed62d26
Luu Duc Trung <luuductrung1234@gmail.com>
2020-05-17 16:16:16 +0700

    update serialize methods to generic

--- LearnSerialization/com/learning/Main.java
+++ LearnSerialization/com/learning/Main.java
@@ -6,25 +6,59 @@ import java.io.ObjectOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import com.learning.AccountGroup;
+import com.learning.BankAccount;
 
 public class Main {
     private static Logger logger = Logger.getLogger(Main.class.getSimpleName());
 
     public static void main(String[] args) {
-        BankAccount bankAccount = new BankAccount("1234", 500);
-        bankAccount.deposit(100);
-        saveAccount(bankAccount, "assets/account.dat");
+        demoPersistingObject(new BankAccount("1234", 500), "assets/account.dat");
 
-        BankAccount persistedAccount = loadAccount("assets/account.dat");
+        List<BankAccount> accounts = new ArrayList<>();
+        accounts.add(new BankAccount("1234", 400));
+        accounts.add(new BankAccount("3409", 280));
+        demoTransientFields(accounts, "assets/group.dat");
+    }
+
+    private static void demoPersistingObject(BankAccount bankAccount, String fileName) {
+        bankAccount.deposit(100);
+        saveObject(bankAccount, fileName);
+
+        BankAccount persistedAccount = loadObject(fileName, BankAccount.class);
         if (persistedAccount != null)
             logger.log(Level.INFO, "Loaded account id:{0}, balance:{1}, lastTxType:{2}, lastTxAmount:{3}",
                     new Object[] { persistedAccount.getId(), persistedAccount.getBalance(),
                             persistedAccount.getLastTxType(), persistedAccount.getLastTxAmount() });
     }
 
-    public static void saveAccount(BankAccount bankAccount, String fileName) {
+    private static void demoTransientFields(List<BankAccount> accounts, String fileName) {
+        AccountGroup accountGroup = new AccountGroup();
+        for (BankAccount account : accounts) {
+            accountGroup.addAccount(account);
+        }
+        saveObject(accountGroup, fileName);
+
+        AccountGroup persistedGroup = loadObject(fileName, AccountGroup.class);
+        if (persistedGroup != null) {
+            var persistedAccounts = persistedGroup.getAccountMap().values();
+            logger.log(Level.INFO, "This group contains {0} accounts with total balance is {1}",
+                    new Object[] { persistedAccounts.size(), persistedGroup.getTotalBalance() });
+
+            for (BankAccount persistedAccount : persistedAccounts) {
+                logger.log(Level.INFO, "Loaded account id:{0}, balance:{1}, lastTxType:{2}, lastTxAmount:{3}",
+                        new Object[] { persistedAccount.getId(), persistedAccount.getBalance(),
+                                persistedAccount.getLastTxType(), persistedAccount.getLastTxAmount() });
+            }
+        }
+    }
+
+    private static <T> void saveObject(T bankAccount, String fileName) {
         try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(
                 Files.newOutputStream(Paths.get(fileName), StandardOpenOption.CREATE))) {
             objectOutputStream.writeObject(bankAccount);
@@ -33,11 +67,11 @@ public class Main {
         }
     }
 
-    public static BankAccount loadAccount(String fileName) {
-        BankAccount account = null;
+    private static <T> T loadObject(String fileName, Class<T> resultType) {
+        T account = null;
         try (ObjectInputStream objectInputStream = new ObjectInputStream(
                 Files.newInputStream(Paths.get(fileName), StandardOpenOption.CREATE))) {
-            account = (BankAccount) objectInputStream.readObject();
+            account = resultType.cast(objectInputStream.readObject());
         } catch (Exception e) {
             logger.log(Level.SEVERE, "{0} - {1}", new Object[] { e.getClass().getSimpleName(), e.getMessage() });
         }
--- build-learnserialization.sh
+++ build-learnserialization.sh
@@ -2,6 +2,8 @@
 cd LearnSerialization
 
 javac com/learning/Main.java
+javac com/learning/BankAccount.java
+javac com/learning/AccountGroup.java
 
 echo "Build Learn Serialization Completed!"
 
