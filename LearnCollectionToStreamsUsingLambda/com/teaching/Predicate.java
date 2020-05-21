1fde4729090ff25a16e71ec489e8c67a95c3c7e6
Luu Duc Trung <luuductrung1234@gmail.com>
2020-05-21 22:30:03 +0700

    Predicate function interface

---
+++ LearnCollectionToStreamsUsingLambda/com/teaching/Predicate.java
@@ -0,0 +1,18 @@
+package com.teaching;
+
+@FunctionalInterface
+public interface Predicate<T> {
+    public boolean test(T t);
+
+    public default Predicate<T> and(Predicate<T> other) {
+        return t -> this.test(t) && other.test(t);
+    }
+
+    public default Predicate<T> or(Predicate<T> other) {
+        return t -> this.test(t) || other.test(t);
+    }
+
+    public static <T> Predicate<T> isEqualsTo(T value) {
+        return t -> t.equals(value);
+    }
+}
