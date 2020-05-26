700cbfa1a3f0e9e9ef77869732ba153d2dceda0e
Trung Luu <luu.trung@logixtek.com>
2020-05-26 13:44:24 +0700

    the spliterator patterns

---
+++ build/build-dataprocessinginjava8.sh
@@ -0,0 +1,10 @@
+
+cd DataProcessingInJava8
+
+javac com/spliteratorPattern/Main.java
+javac com/spliteratorPattern/Person.java
+javac com/spliteratorPattern/PersonSpliterator.java
+
+echo "Build Streams, Collectors, Optionals for Data Processing in Java8  Completed!"
+
+cd ..
