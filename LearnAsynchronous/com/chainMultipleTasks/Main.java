38cc7929179922f991e0a8a61e8133e7c05768dd
Trung Luu <luu.trung@logixtek.com>
2020-06-25 14:29:51 +0700

    determine which thread chain of tasks are executed

--- LearnAsynchronous/com/chainMultipleTasks/Main.java
+++ LearnAsynchronous/com/chainMultipleTasks/Main.java
@@ -3,6 +3,8 @@ package com.chainMultipleTasks;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.logging.Level;
@@ -10,8 +12,10 @@ import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import util.logging.CustomLogger;
+
 public class Main {
-    private static Logger logger = Logger.getLogger(Main.class.getSimpleName());
+    private static Logger logger = CustomLogger.getLogger(Main.class.getSimpleName());
 
     public static void main(String[] args) {
         int choice = 0;
@@ -27,6 +31,9 @@ public class Main {
                 break;
             case 2:
                 composingCompletableFuture();
+                break;
+            case 3:
+                threadToRunTriggeredTask();
                 break;
             default:
                 supplyResultToFunctionDemo();
@@ -60,7 +67,7 @@ public class Main {
          * getUsersByIds() is a synchronous function, it conducted synchronously when
          * the list of user IDs is available
          */
-        logger.log(Level.INFO, "Run synchronous chain of tasks");
+        logger.log(Level.INFO, "RUN SYNCHRONOUS CHAIN OF TASKS");
         CompletableFuture<List<User>> completableFuture = CompletableFuture.supplyAsync(getUserIds)
                 .thenApply(getUsersByIds);
         completableFuture.thenAccept(users -> logger.log(Level.INFO, "{0} users have been read", users.size()));
@@ -73,11 +80,72 @@ public class Main {
          * 
          * CompletableFuture.thenCompose() work the same concept as Stream.flatMap()
          */
-        logger.log(Level.INFO, "Run asynchronous chain of tasks");
+        logger.log(Level.INFO, "RUN ASYNCHRONOUS CHAIN OF TASKS");
         Function<List<Long>, CompletableFuture<List<User>>> getUsersByIdsAsync = ids -> fetchUsersFromDBAsync(ids);
         completableFuture = CompletableFuture.supplyAsync(getUserIds).thenCompose(getUsersByIdsAsync);
         completableFuture.thenAccept(users -> logger.log(Level.INFO, "{0} users have been read", users.size()));
         completableFuture.join();
+    }
+
+    /**
+     * Which thread a triggered task is executed
+     */
+    private static void threadToRunTriggeredTask() {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+        /**
+         * Two task are executed in the same thread of given pool
+         */
+        logger.log(Level.INFO, "TWO TASK IN SAME THREAD OF GIVEN POOL");
+        CompletableFuture<List<User>> cf1 = CompletableFuture.supplyAsync(() -> {
+            logger.log(Level.INFO, "Supplier run thread: {0}", Thread.currentThread().getName());
+            return Stream.of(1L, 2L, 3L).collect(Collectors.toList());
+        }, executorService).thenApply((list) -> {
+            logger.log(Level.INFO, "Function run thread: {0}", Thread.currentThread().getName());
+            return fetchUsersFromDB(list);
+        });
+        cf1.join();
+
+        /**
+         * Two task are executed in the same thread of Common Fork/Join pool
+         */
+        logger.log(Level.INFO, "TWO TASK IN SAME THREAD OF COMMON FORK/JOIN POOL");
+        CompletableFuture<List<User>> cf2 = CompletableFuture.supplyAsync(() -> {
+            logger.log(Level.INFO, "Supplier run thread: {0}", Thread.currentThread().getName());
+            return Stream.of(1L, 2L, 3L).collect(Collectors.toList());
+        }).thenApply((list) -> {
+            logger.log(Level.INFO, "Function run thread: {0}", Thread.currentThread().getName());
+            return fetchUsersFromDB(list);
+        });
+        cf2.join();
+
+        /**
+         * Two task are executed in different threads of given pool
+         */
+        logger.log(Level.INFO, "TWO TASK IN DIFFERENT THREAD OF GIVEN POOL");
+        CompletableFuture<List<User>> cf3 = CompletableFuture.supplyAsync(() -> {
+            logger.log(Level.INFO, "Supplier run thread: {0}", Thread.currentThread().getName());
+            return Stream.of(1L, 2L, 3L).collect(Collectors.toList());
+        }, executorService).thenApplyAsync((list) -> {
+            logger.log(Level.INFO, "Function run thread: {0}", Thread.currentThread().getName());
+            return fetchUsersFromDB(list);
+        }, executorService);
+        cf3.join();
+
+        /**
+         * Two task are executed in different threads of Common Fork/Join pool
+         */
+        logger.log(Level.INFO, "TWO TASK IN DIFFERENT THREAD OF COMMON FORK/JOIN POOL");
+        CompletableFuture<List<User>> cf4 = CompletableFuture.supplyAsync(() -> {
+            logger.log(Level.INFO, "Supplier run thread: {0}", Thread.currentThread().getName());
+            return Stream.of(1L, 2L, 3L).collect(Collectors.toList());
+        }).thenApplyAsync((list) -> {
+            logger.log(Level.INFO, "Function run thread: {0}", Thread.currentThread().getName());
+            return fetchUsersFromDB(list);
+        });
+        cf4.join();
+
+        executorService.shutdown();
     }
 
     // ********************************************
